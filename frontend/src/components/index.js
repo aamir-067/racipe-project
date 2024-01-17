dc3a472db375c54db7d0606d807bc143f435b9d9
xKhanDev <safeerkhanxofficial@gmail.com>
2024-01-17 10:19:46 -0800

    Setting directories

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,21 @@
-// import each component here and then export it.
+// Import each component here
+import NavBar from "./NavBar/NavBar";
+import Header from "./Header/Header";
+import EditProfile from "./EditProfile/EditProfile";
+import EditRecipe from "./EditRecipe/EditRecipe";
+import AllRecipes from "./AllRecipes/AllRecipes";
+import ProfilePreview from "./ProfilePreview/ProfilePreview";
+import RecipeCard from "./RecipeCard/RecipeCard";
+import RecipePreview from "./RecipePreview/RecipePreview";
+
+// Export each component without the default keyword
+export {
+  NavBar,
+  Header,
+  EditProfile,
+  EditRecipe,
+  AllRecipes,
+  ProfilePreview,
+  RecipeCard,
+  RecipePreview,
+};
