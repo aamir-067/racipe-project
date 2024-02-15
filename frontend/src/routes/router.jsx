1746a6bf0e05dccee4dedc6199426159d9beefc3
aamir <aamirkhan@engineer.com>
2024-02-15 11:34:58 +0500

    added registration and login routes

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,10 +1,14 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import App from "../App.jsx";
+import { Registration, Login, RecipePreview } from "../components/index.js";
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/">
             <Route path="" element={<App />} />
-            <Route path="/dashboard" element={<h2>hello</h2>} />
+            {/* <Route path="/dashboard" element={<h2>hello</h2>} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/:recipeId/details" element={<Registration />} />
         </Route>
     )
 );
