650da486449273061299b43ae46f756e6b88fe31
aamir <aamirkhan@engineer.com>
2024-11-01 10:55:34 +0500

    vercel fix

---
+++ frontend/src/CONSTANTS.js
@@ -0,0 +1,4 @@
+export const TEST = "HELLO";
+export const temp = "temp"
+// export const serverApi = "http://localhost:3000/api/v1/";
+export const serverApi = "https://racipe-project.vercel.app/api/v1/";
